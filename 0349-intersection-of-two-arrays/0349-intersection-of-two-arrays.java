615f4755db633ef353fc44245b7aaac7ad100ad7
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-07-06 20:55:59 +0530

    Time: 2 ms (98.05%), Space: 45 MB (56.81%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -1,23 +1,44 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
+        // **** Brute force approach****
+        // HashSet<Integer> set = new HashSet<>();
 
-        HashSet<Integer> set = new HashSet<>();
+        //  for(int i = 0; i < nums1.length; i++){
+        //     for(int j = 0; j < nums2.length; j++){
+        //         if(nums1[i] == nums2[j]){
+        //             set.add(nums1[i]);
+        //         }
+        //     }
+        //  }
+        //  int[] arr = new int[set.size()];
 
-         for(int i = 0; i < nums1.length; i++){
-            for(int j = 0; j < nums2.length; j++){
-                if(nums1[i] == nums2[j]){
-                    set.add(nums1[i]);
-                }
-            }
-         }
-         int[] arr = new int[set.size()];
+        //  int index = 0;
+        //  for(int i : set){
+        //     arr[index] = i;
+        //     index++;
+        //  }
+        //  return arr;
+    //**** */ Optimal approach ****
 
-         int index = 0;
-         for(int i : set){
-            arr[index] = i;
-            index++;
-         }
-         return arr;
+    Set<Integer> set1 = new HashSet<>();
+    Set<Integer> set2 = new HashSet<>();
 
+    for(int i : nums1){
+        if(!set1.contains(i)){
+            set1.add(i);
+        }
+    }
+    for(int j : nums2){
+        if(set1.contains(j)){
+            set2.add(j);
+        }
+    }
+    int[] arr = new int[set2.size()];
+    int index = 0;
+    for(int i : set2){
+        arr[index] = i;
+        index++;
+    }
+    return arr;
     }
 }
