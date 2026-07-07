8977efafc527aa984fafb3d31cbcc4f4f3816e84
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-07-08 01:25:03 +0530

    Time: 3 ms (72.8%), Space: 46.5 MB (80.09%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -4,47 +4,67 @@ class Solution {
 
         char[] arr = s.toCharArray();
         int i = 0; 
-        int j = arr.length-1;
+        int j = s.length()-1;
 
-        String result = "";
+       // String result = "";
 
-        while(i < j){
+    //     while(i < j){
 
-        if((arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U' || arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') 
-        && 
-        (arr[j] == 'A' || arr[j] == 'E' || arr[j] == 'I' || arr[j] == 'O' || arr[j] == 'U' || arr[j] == 'a' || arr[j] == 'e' || arr[j] == 'i' || arr[j] == 'o' || arr[j] == 'u')){
+    //     if((arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U' || arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') 
+    //     && 
+    //     (arr[j] == 'A' || arr[j] == 'E' || arr[j] == 'I' || arr[j] == 'O' || arr[j] == 'U' || arr[j] == 'a' || arr[j] == 'e' || arr[j] == 'i' || arr[j] == 'o' || arr[j] == 'u')){
+    //         char temp = arr[i];
+    //         arr[i] = arr[j];
+    //         arr[j] = temp;
+    //         i++;
+    //         j--;
+    //     }
+
+        
+    //     else if((arr[i] != 'A' || arr[i] != 'E' || arr[i] != 'I' || arr[i] != 'O' || arr[i] != 'U' || arr[i] != 'a' || arr[i] != 'e' || arr[i] != 'i' || arr[i] != 'o' || arr[i] != 'u') 
+    //     && 
+    //     (arr[j] == 'A' || arr[j] == 'E' || arr[j] == 'I' || arr[j] == 'O' || arr[j] == 'U' || arr[j] == 'a' || arr[j] == 'e' || arr[j] == 'i' || arr[j] == 'o' || arr[j] == 'u')){
+    //         i++;
+    //     }
+
+
+    //     else if((arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U' || arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') 
+    //     && 
+    //     (arr[j] != 'A' || arr[j] != 'E' || arr[j] != 'I' || arr[j] != 'O' || arr[j] != 'U' || arr[j] != 'a' || arr[j] != 'e' || arr[j] != 'i' || arr[j] != 'o' || arr[j] != 'u')){
+    //         j--;
+    //     }
+    //     else{
+    //         i++;
+    //         j--;
+    //     }
+
+       
+        
+    // }
+    //  for(char nums : arr){
+    //         result += nums;
+    //     }
+    // return result;
+
+
+    String vowel = "aeiouAEIOU";
+
+    while(i < j){
+        while(i < j && vowel.indexOf(arr[i]) == -1){
+            i++;
+        }
+        while(i < j && vowel.indexOf(arr[j]) == -1){
+            j--;
+        }
+        if(i < j){
             char temp = arr[i];
             arr[i] = arr[j];
             arr[j] = temp;
             i++;
             j--;
         }
-
-        
-        else if((arr[i] != 'A' || arr[i] != 'E' || arr[i] != 'I' || arr[i] != 'O' || arr[i] != 'U' || arr[i] != 'a' || arr[i] != 'e' || arr[i] != 'i' || arr[i] != 'o' || arr[i] != 'u') 
-        && 
-        (arr[j] == 'A' || arr[j] == 'E' || arr[j] == 'I' || arr[j] == 'O' || arr[j] == 'U' || arr[j] == 'a' || arr[j] == 'e' || arr[j] == 'i' || arr[j] == 'o' || arr[j] == 'u')){
-            i++;
-        }
-
-
-        else if((arr[i] == 'A' || arr[i] == 'E' || arr[i] == 'I' || arr[i] == 'O' || arr[i] == 'U' || arr[i] == 'a' || arr[i] == 'e' || arr[i] == 'i' || arr[i] == 'o' || arr[i] == 'u') 
-        && 
-        (arr[j] != 'A' || arr[j] != 'E' || arr[j] != 'I' || arr[j] != 'O' || arr[j] != 'U' || arr[j] != 'a' || arr[j] != 'e' || arr[j] != 'i' || arr[j] != 'o' || arr[j] != 'u')){
-            j--;
-        }
-        else{
-            i++;
-            j--;
-        }
-
-       
-        
     }
-     for(char nums : arr){
-            result += nums;
-        }
-    return result;
+    return new String(arr);
     }
     
 }
