bb3337566d492ed35b182746732d9e81c57a338a
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-07-11 02:13:35 +0530

    Time: 1 ms (97.23%), Space: 42.8 MB (81.49%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -23,63 +23,75 @@ class Solution {
         // }
         // return count;
 
-        //****** */ Optimal approach*******
+        //****** */ Better approach*******
 
 
-        int k = 3;
-        HashMap<Character, Integer> map = new HashMap<>();
-        int count = 0;
-        boolean flag = true;
+        // int k = 3;
+        // HashMap<Character, Integer> map = new HashMap<>();
+        // int count = 0;
+        // boolean flag = true;
 
-        if(s.length() < k){
-            return 0;
-        }
-        for(int i = 0; i < k; i++){
+        // if(s.length() < k){
+        //     return 0;
+        // }
+        // for(int i = 0; i < k; i++){
 
-           if(map.containsKey(s.charAt(i))){
-            int temp = map.get(s.charAt(i));
-            map.put(s.charAt(i), temp+1);
-           }
-           else{
-                 map.put(s.charAt(i), 1);
-           }
+        //    if(map.containsKey(s.charAt(i))){
+        //     int temp = map.get(s.charAt(i));
+        //     map.put(s.charAt(i), temp+1);
+        //    }
+        //    else{
+        //          map.put(s.charAt(i), 1);
+        //    }
           
            
 
-        }
-        if(map.size() == 3){
-            count++;
-        }
+        // }
+        // if(map.size() == 3){
+        //     count++;
+        // }
 
-        for(int j = k; j <= s.length()-1; j++){
-           int cw = map.get(s.charAt(j-k));
+        // for(int j = k; j <= s.length()-1; j++){
+        //    int cw = map.get(s.charAt(j-k));
            
-            if(cw >= 2){
-                map.put(s.charAt(j-k), cw-1);
-            }
-            else{
-                map.remove(s.charAt(j-k));
-            }
+        //     if(cw >= 2){
+        //         map.put(s.charAt(j-k), cw-1);
+        //     }
+        //     else{
+        //         map.remove(s.charAt(j-k));
+        //     }
 
             
-            if(map.containsKey(s.charAt(j))){
+        //     if(map.containsKey(s.charAt(j))){
                 
-                int temp = map.get(s.charAt(j));
-                map.put(s.charAt(j), temp+1);
-            }
-            else{
-                map.put(s.charAt(j), 1);
-            }
+        //         int temp = map.get(s.charAt(j));
+        //         map.put(s.charAt(j), temp+1);
+        //     }
+        //     else{
+        //         map.put(s.charAt(j), 1);
+        //     }
 
-            if(map.size() == 3){
-            count++;
+        //     if(map.size() == 3){
+        //     count++;
 
-            }
+        //     }
             
-        }
-        return count;
+        // }
+        // return count;
         
+// *********** Optimal Approach **********
 
-        
+int count=0;
+
+for(int i = 0; i <= s.length()-3; i++){
+    char a = s.charAt(i);
+    char b = s.charAt(i+1);
+    char c = s.charAt(i+2);
+
+    if(a != b && b != c && a != c){
+        count++;
+    }
+}
+        return count;
     }
 }
