469f60a18d715b604acf99517bf6565238b77c0d
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-07-10 23:02:01 +0530

    Time: 3 ms (86.02%), Space: 71.7 MB (80.28%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.java
@@ -2,19 +2,48 @@ class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
         // brute force approach
 
+        // int count = 0;
+        // int average = 0;
+        // for(int i = 0; i <= arr.length-k; i++){
+        //     int sum = 0;
+        //     for(int j = i; j < i+k; j++){
+        //         sum+=arr[j];
+                
+        //     }
+        //     average = sum / k;
+        //         if(average >= threshold){
+        //             count++;
+        //         }
+        // }
+        // return count;
+
+//********* Optimal Approach ********** */
+
+
         int count = 0;
         int average = 0;
-        for(int i = 0; i <= arr.length-k; i++){
-            int sum = 0;
-            for(int j = i; j < i+k; j++){
-                sum+=arr[j];
-                
-            }
+        int sum = 0;
+
+        for(int i = 0; i < k; i++){
+            sum+=arr[i];
+            
+        }
             average = sum / k;
-                if(average >= threshold){
-                    count++;
-                }
+            if(average >= threshold){
+                count++;
+            }
+        for(int j = k; j < arr.length; j++){
+            sum += arr[j];
+            sum -= arr[j-k];
+
+            average = sum / k;
+
+            if(average >= threshold){
+                count++;
+            }
         }
         return count;
+
+
     }
 }
