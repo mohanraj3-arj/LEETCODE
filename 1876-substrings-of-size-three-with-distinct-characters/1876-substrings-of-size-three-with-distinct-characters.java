92f16a61511fdca5cdcc077219c9cba35f79e595
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-07-11 02:08:50 +0530

    Time: 4 ms (19.29%), Space: 44.2 MB (16.68%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.java
@@ -1,27 +1,85 @@
 class Solution {
     public int countGoodSubstrings(String s) {
-
-        int count = 0;
-        int k = 3;
+// ********* brute force approach ********
+        // int count = 0;
+        // int k = 3;
         
-        HashSet<Character> set = new HashSet<>();
-        for(int i = 0; i <= s.length()-k; i++){
+        // HashSet<Character> set = new HashSet<>();
+        // for(int i = 0; i <= s.length()-k; i++){
             
-            set.clear();
-            boolean flag = true;
-            for(int j = i; j < i+k; j++){
-                if(set.contains(s.charAt(j))){
-                    flag = false;
-                }
+        //     set.clear();
+        //     boolean flag = true;
+        //     for(int j = i; j < i+k; j++){
+        //         if(set.contains(s.charAt(j))){
+        //             flag = false;
+        //         }
 
-                set.add(s.charAt(j));
+        //         set.add(s.charAt(j));
+        //     }
+        //     if(flag==true){
+        //         count++;
+        //     }
+            
+        // }
+        // return count;
+
+        //****** */ Optimal approach*******
+
+
+        int k = 3;
+        HashMap<Character, Integer> map = new HashMap<>();
+        int count = 0;
+        boolean flag = true;
+
+        if(s.length() < k){
+            return 0;
+        }
+        for(int i = 0; i < k; i++){
+
+           if(map.containsKey(s.charAt(i))){
+            int temp = map.get(s.charAt(i));
+            map.put(s.charAt(i), temp+1);
+           }
+           else{
+                 map.put(s.charAt(i), 1);
+           }
+          
+           
+
+        }
+        if(map.size() == 3){
+            count++;
+        }
+
+        for(int j = k; j <= s.length()-1; j++){
+           int cw = map.get(s.charAt(j-k));
+           
+            if(cw >= 2){
+                map.put(s.charAt(j-k), cw-1);
             }
-            if(flag==true){
-                count++;
+            else{
+                map.remove(s.charAt(j-k));
+            }
+
+            
+            if(map.containsKey(s.charAt(j))){
+                
+                int temp = map.get(s.charAt(j));
+                map.put(s.charAt(j), temp+1);
+            }
+            else{
+                map.put(s.charAt(j), 1);
+            }
+
+            if(map.size() == 3){
+            count++;
+
             }
             
         }
         return count;
+        
+
         
     }
 }
