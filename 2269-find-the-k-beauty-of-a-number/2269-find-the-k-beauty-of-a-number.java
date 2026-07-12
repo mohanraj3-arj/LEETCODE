c6372f4ba94a31c8c0dbaab2d4dbe7c69e84dac3
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-07-12 15:54:48 +0530

    Time: 4 ms (8.93%), Space: 42.2 MB (55.97%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -2,28 +2,65 @@ class Solution {
     public int divisorSubstrings(int num, int k) {
         String nums1 = Integer.toString(num);
 
-        int count = 0;
-        int x = 0;
+        // int count = 0;
+        // int x = 0;
         
 
-        if(nums1.length() == k){
+        
+        // for(int i = 0; i <= nums1.length()-k; i++){
+        //     String temp = "";
+        //     for(int j = i; j < i+k; j++){
+                
+        //         temp += nums1.charAt(j);
+  
+        //     }
+        //       x = Integer.parseInt(temp);
+        //     if(x != 0){
+        //         if(num % x == 0){
+        //         count++;
+        //        }
+        //     }
+        // }
+        // return count;
+
+
+
+
+        int count = 0;
+        String temp = "";
+
+         if(nums1.length() == k){
            if(num % num == 0){
             count++;
             return count;
            }
         }
-        for(int i = 0; i <= nums1.length()-k; i++){
-            String temp = "";
-            for(int j = i; j < i+k; j++){
-                
-                temp += nums1.charAt(j);
-  
+
+        for(int i = 0; i < k; i++){
+            temp += nums1.charAt(i);
+        }
+        int x = Integer.parseInt(temp);
+        if(x != 0){
+            if(num % x == 0){
+                count++;
             }
-              x = Integer.parseInt(temp);
+        }
+        for(int j = k; j < nums1.length(); j++){
+            
+       
+               temp += nums1.charAt(j);
+                temp = temp.substring(1);
+            
+           
+         if(temp.length() != 0){
+             x = Integer.parseInt(temp);
+
+         }
+           
             if(x != 0){
                 if(num % x == 0){
-                count++;
-               }
+                    count++;
+                }
             }
         }
         return count;
