c79c8fecadeadc5698f0f4a8e6b6cc620e122088
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-07-12 16:12:20 +0530

    Time: 1 ms (93.16%), Space: 42.2 MB (73.11%) - LeetHub

--- 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
+++ 2269-find-the-k-beauty-of-a-number/2269-find-the-k-beauty-of-a-number.java
@@ -2,6 +2,38 @@ class Solution {
     public int divisorSubstrings(int num, int k) {
         String nums1 = Integer.toString(num);
 
+//****** Optimal Approach ********* */
+
+
+
+    int beauty = 0;
+    var StrNum = String.valueOf(num);
+
+    for(int i = 0; i <= StrNum.length()-k; i++){
+        var StrNumSub = Integer.parseInt(StrNum.substring(i, i+k));
+
+        if(StrNumSub > 0 && num % StrNumSub == 0){
+            beauty++;
+        }
+    }
+    return beauty;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // ********* Brute force approach ***********
+
         // int count = 0;
         // int x = 0;
         
@@ -24,46 +56,46 @@ class Solution {
         // return count;
 
 
+// ************ better approach ***************
 
+        // int count = 0;
+        // String temp = "";
 
-        int count = 0;
-        String temp = "";
+        //  if(nums1.length() == k){
+        //    if(num % num == 0){
+        //     count++;
+        //     return count;
+        //    }
+        // }
 
-         if(nums1.length() == k){
-           if(num % num == 0){
-            count++;
-            return count;
-           }
-        }
-
-        for(int i = 0; i < k; i++){
-            temp += nums1.charAt(i);
-        }
-        int x = Integer.parseInt(temp);
-        if(x != 0){
-            if(num % x == 0){
-                count++;
-            }
-        }
-        for(int j = k; j < nums1.length(); j++){
+        // for(int i = 0; i < k; i++){
+        //     temp += nums1.charAt(i);
+        // }
+        // int x = Integer.parseInt(temp);
+        // if(x != 0){
+        //     if(num % x == 0){
+        //         count++;
+        //     }
+        // }
+        // for(int j = k; j < nums1.length(); j++){
             
        
-               temp += nums1.charAt(j);
-                temp = temp.substring(1);
+        //        temp += nums1.charAt(j);
+        //         temp = temp.substring(1);
             
            
-         if(temp.length() != 0){
-             x = Integer.parseInt(temp);
+        //  if(temp.length() != 0){
+        //      x = Integer.parseInt(temp);
 
-         }
+        //  }
            
-            if(x != 0){
-                if(num % x == 0){
-                    count++;
-                }
-            }
-        }
-        return count;
+        //     if(x != 0){
+        //         if(num % x == 0){
+        //             count++;
+        //         }
+        //     }
+        // }
+        // return count;
         
     }
 }
