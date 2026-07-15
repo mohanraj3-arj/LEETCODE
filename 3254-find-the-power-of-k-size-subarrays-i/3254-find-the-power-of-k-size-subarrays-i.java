dfb184d4058664675ae26aff1f70934f6eb85248
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-07-16 01:58:18 +0530

    Time: 1 ms (100%), Space: 47.5 MB (35.08%) - LeetHub

--- 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
+++ 3254-find-the-power-of-k-size-subarrays-i/3254-find-the-power-of-k-size-subarrays-i.java
@@ -1,45 +1,91 @@
 class Solution {
     public int[] resultsArray(int[] nums, int k) {
-        int n = nums.length;
-        int[] result = new int[n - k + 1];
-        Arrays.fill(result, -1);
-        if(nums.length == k && nums.length == 1){
-            return nums;
-        }
-        if(k == 1){
-            return nums;
-        }
 
-        for(int i = 0; i <= nums.length-k; i++){
-            int max = i;
-            boolean flag = true;
+    // ** brute force approach **
+
+        // int n = nums.length;
+        // int[] result = new int[n - k + 1];
+        // Arrays.fill(result, -1);
+        // if(nums.length == k && nums.length == 1){
+        //     return nums;
+        // }
+        // if(k == 1){
+        //     return nums;
+        // }
+
+        // for(int i = 0; i <= nums.length-k; i++){
+        //     int max = i;
+        //     boolean flag = true;
           
-            for(int j = i; j < (i + k) - 1; j++){
-                if(nums[j] > nums[j+1]){
-                    flag = false;
-                }
-                int diff = Math.abs(nums[j+1] - nums[j]);
-                if(diff != 1){
-                    flag = false;
-                }
-                else{
+        //     for(int j = i; j < (i + k) - 1; j++){
+        //         if(nums[j] > nums[j+1]){
+        //             flag = false;
+        //         }
+        //         int diff = Math.abs(nums[j+1] - nums[j]);
+        //         if(diff != 1){
+        //             flag = false;
+        //         }
+        //         else{
                     
-                    max = j + 1;
+        //             max = j + 1;
                    
-                }
+        //         }
                 
-            }
-            if(flag){
-                if(nums[max] != nums[i]){
-                    result[i] = nums[max];
-                }
+        //     }
+        //     if(flag){
+        //         if(nums[max] != nums[i]){
+        //             result[i] = nums[max];
+        //         }
 
                
                 
                 
+        //     }
+        // }
+        // return result;
+
+    // **Optimal approach **
+
+
+
+
+       
+        int n = nums.length;
+        int [] result = new int[n - k + 1];
+
+        int consecutive  = 0;
+
+        for(int i = 0; i < k-1; i++){
+            if(nums[i] + 1 == nums[i+1]){
+                consecutive++;
             }
         }
-        return result;
+
+        if(consecutive == k - 1){
+            result[0] = nums[k-1];
+
+        }
+        else{
+            result[0] = -1;
+        }
+        for(int i = 1; i <= n - k; i++){
+            if(nums[i] == nums[i-1] + 1){
+                consecutive--;
+            }
+            if(nums[i + k - 1] == nums[i + k - 2] + 1){
+                consecutive++;
+            }
+            if(consecutive == k - 1){
+                result[i] = nums[i + k - 1];
+            }
+            else{
+                result[i] = -1;
+            }
+        }
+
         
+    return result;
+
+
     }
 }
