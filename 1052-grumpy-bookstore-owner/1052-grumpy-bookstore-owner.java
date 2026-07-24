745de4ea0ea1ec05e1fa3a4a2d7b5200faba1ec9
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-07-25 00:33:23 +0530

    Time: 4 ms (32.55%), Space: 48.9 MB (18.31%) - LeetHub

--- 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
+++ 1052-grumpy-bookstore-owner/1052-grumpy-bookstore-owner.java
@@ -2,33 +2,85 @@ class Solution {
     public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
 
         
-        int max = -1;
-        int n = customers.length;
-        int index = 0;
+        // int max = -1;
+        // int n = customers.length;
+        // int index = 0;
 
-        for(int i = 0; i <= n-minutes; i++){
-            int sum = 0;
+        // for(int i = 0; i <= n-minutes; i++){
+        //     int sum = 0;
 
-            for(int j = i; j < i+minutes; j++){
-                if(grumpy[j] == 1){
-                    sum+=customers[j];
-                }
-            }
-            if(sum > max){
-                max = sum;
-                index = i;
-            }
+        //     for(int j = i; j < i+minutes; j++){
+        //         if(grumpy[j] == 1){
+        //             sum+=customers[j];
+        //         }
+        //     }
+        //     if(sum > max){
+        //         max = sum;
+        //         index = i;
+        //     }
+        // }
+        // for(int i = index; i <= index + minutes - 1; i++){
+        //     grumpy[i] = 0;
+        // }
+        // int satisfiedCustomers = 0;
+        // for(int j = 0; j < n; j++){
+        //     if(grumpy[j] == 0){
+        //         satisfiedCustomers += customers[j];
+        //     }
+        // }
+        // return satisfiedCustomers;
+
+        // **** Optimal Approach ****
+
+
+    int max = Integer.MIN_VALUE;
+    int sum = 0;
+    
+    for(int i = 0; i < minutes; i++){
+        if(grumpy[i] == 1){
+            sum += customers[i];
         }
-        for(int i = index; i <= index + minutes - 1; i++){
-            grumpy[i] = 0;
-        }
-        int satisfiedCustomers = 0;
-        for(int j = 0; j < n; j++){
-            if(grumpy[j] == 0){
-                satisfiedCustomers += customers[j];
+    }
+    max = sum;
+    for(int j = minutes; j < customers.length; j++){
+            if(grumpy[j-minutes]  == 1){
+                sum -= customers[j-minutes];
+
             }
-        }
-        return satisfiedCustomers;
+            if(grumpy[j] == 1){
+                sum += customers[j];
+
+            }
+                
+           max = Math.max(max, sum);
+          
+    }
+
+int satisfiedCustomer = 0;
+for(int i = 0; i < customers.length; i++){
+    if(grumpy[i] == 0){
+         satisfiedCustomer += customers[i];
+    }
+   
+}
+    return satisfiedCustomer + max;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
     }
 }
