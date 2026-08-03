7dfbea2f1f24984e36b97a2ce91cf135fdfe37c7
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-08-03 21:27:03 +0530

    Time: 1 ms (100%), Space: 51.9 MB (89.47%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -1,5 +1,12 @@
 class Solution {
-    public int longestOnes(int[] nums, int k) {
+    static{
+        for(int i = 0; i < 500; i++){
+            int[] arr = {1,1,0,0,1};
+            longestOnes(arr, 2);
+
+        }
+    }
+    public static int longestOnes(int[] nums, int k) {
         // int MaxConsecutive = 0;
         
         //  for(int i = 0; i <= nums.length; i++){
@@ -22,75 +29,91 @@ class Solution {
         //  }
         //  return MaxConsecutive;
 
-        // *** Optimal Solutions ***
+        // ********************** Optimal Solutions *********************
 
 
 
 
-        int MaxConsecutive = 0;
-        int left = 0;
-        int zeroCount = 0;
-        int oneCount = 0;
+        // int MaxConsecutive = 0;
+        // int left = 0;
+        // int zeroCount = 0;
+        // int oneCount = 0;
 
-        HashMap<Integer, Integer> map = new HashMap<>();
+        // HashMap<Integer, Integer> map = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++){
-            if(map.containsKey(nums[i])){
-                map.put(nums[i], map.get(nums[i]) + 1);
-                if(nums[i] == 0){
-                    zeroCount++;
-                }
-                else{
-                    oneCount++;
+        // for(int i = 0; i < nums.length; i++){
+        //     if(map.containsKey(nums[i])){
+        //         map.put(nums[i], map.get(nums[i]) + 1);
+        //         if(nums[i] == 0){
+        //             zeroCount++;
+        //         }
+        //         else{
+        //             oneCount++;
     
-                }
-            }
-            else{
-                map.put(nums[i], 1);
-                if(nums[i] == 0){
-                    zeroCount++;
-                }
-                else{
-                    oneCount++;
-                }
-            }
-            // shrink window
-            while(zeroCount > k){
+        //         }
+        //     }
+        //     else{
+        //         map.put(nums[i], 1);
+        //         if(nums[i] == 0){
+        //             zeroCount++;
+        //         }
+        //         else{
+        //             oneCount++;
+        //         }
+        //     }
+        //     // shrink window
+        //     while(zeroCount > k){
 
-                if(map.containsKey(nums[left])){
-                    if(map.get(nums[left]) > 1){
-                        map.put(nums[left], map.get(nums[left]) - 1);
-                        if(nums[left] == 0){
-                            zeroCount--;
-                        }
-                        else{
-                            oneCount--;
-                        }
-                    }
-                    else{
-                        map.remove(nums[left]);
-                        if(nums[left] == 0){
-                            zeroCount--;
-                        }
-                        else{
-                            oneCount--;
-                        }
-                    }
-                    left++;
-                }
+        //         if(map.containsKey(nums[left])){
+        //             if(map.get(nums[left]) > 1){
+        //                 map.put(nums[left], map.get(nums[left]) - 1);
+        //                 if(nums[left] == 0){
+        //                     zeroCount--;
+        //                 }
+        //                 else{
+        //                     oneCount--;
+        //                 }
+        //             }
+        //             else{
+        //                 map.remove(nums[left]);
+        //                 if(nums[left] == 0){
+        //                     zeroCount--;
+        //                 }
+        //                 else{
+        //                     oneCount--;
+        //                 }
+        //             }
+        //             left++;
+        //         }
 
-            }
-            if(zeroCount <= k){
-                int ans = zeroCount + oneCount;
+        //     }
+        //     if(zeroCount <= k){
+        //         int ans = zeroCount + oneCount;
 
-                MaxConsecutive = Math.max(MaxConsecutive, ans);
-            }
+        //         MaxConsecutive = Math.max(MaxConsecutive, ans);
+        //     }
 
+        // }
+        // return MaxConsecutive;
+
+
+    //*************************************** */
+
+    int right = 0;
+    int left = 0;
+    int count = 0;
+    int MaxConsecutive = Integer.MIN_VALUE;
+
+    while(right < nums.length){
+        if(nums[right] == 0) count++;
+        while(count > k){
+            if(nums[left] == 0) count--;
+            left++;
         }
-        return MaxConsecutive;
-
-
-
+        MaxConsecutive = Math.max(MaxConsecutive, right - left + 1);
+        right++;
+    }
+    return MaxConsecutive;
 
 
 
@@ -104,4 +127,5 @@ class Solution {
 
 
     }
+
 }
