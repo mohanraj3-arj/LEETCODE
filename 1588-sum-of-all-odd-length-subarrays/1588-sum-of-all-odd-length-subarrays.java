fc8f4a19730f8f597c80b9e564402dc439673543
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-08-11 10:02:29 +0530

    Time: 0 ms (100%), Space: 43.3 MB (24.46%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -18,32 +18,43 @@ class Solution {
       
       // optimal solutions
 
-      int[] prefix = new int[arr.length];
-      prefix[0] = arr[0];
-      int answer = 0;
+    //   int[] prefix = new int[arr.length];
+    //   prefix[0] = arr[0];
+    //   int answer = 0;
 
-      for(int i = 1; i < arr.length; i++){
-        prefix[i] = prefix[i-1] + arr[i];
-      }
+    //   for(int i = 1; i < arr.length; i++){
+    //     prefix[i] = prefix[i-1] + arr[i];
+    //   }
 
-      for(int i = 0; i < arr.length; i++){
+    //   for(int i = 0; i < arr.length; i++){
 
 
-        for(int j = i; j < arr.length; j++){
-            int length = j - i + 1;
-            int sum = 0;
-            if(i > 0){
-                sum = prefix[j] - prefix[i-1];
-            }
-            else{
-                sum = prefix[j];
-            }
+    //     for(int j = i; j < arr.length; j++){
+    //         int length = j - i + 1;
+    //         int sum = 0;
+    //         if(i > 0){
+    //             sum = prefix[j] - prefix[i-1];
+    //         }
+    //         else{
+    //             sum = prefix[j];
+    //         }
              
-            if(length % 2 == 1){
-               answer += sum;
-            }
-        }
+    //         if(length % 2 == 1){
+    //            answer += sum;
+    //         }
+    //     }
+    //   }
+//********************************** */
+
+      int n = arr.length;
+      int answer2 = 0;
+      for(int i = 0; i < arr.length; i++){
+        int right = i + 1;
+        int left = n - i;
+        int total = left * right;
+        int odd = (total + 1) / 2;
+        answer2 += arr[i] * odd;
       }
-      return answer;
+      return answer2;
     }
 }
