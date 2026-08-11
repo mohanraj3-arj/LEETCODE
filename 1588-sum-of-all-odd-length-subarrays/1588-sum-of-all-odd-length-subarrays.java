3a6c29db9c008016832715d78dff257e70cef048
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-08-11 10:02:12 +0530

    Time: 3 ms (37.09%), Space: 43.2 MB (43.29%) - LeetHub

--- 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
+++ 1588-sum-of-all-odd-length-subarrays/1588-sum-of-all-odd-length-subarrays.java
@@ -1,20 +1,49 @@
 class Solution {
-    public int sumOddLengthSubarrays(int[] arr) {
-        int sum = 0;
+     public int sumOddLengthSubarrays(int[] arr) {
+    //     int sum = 0;
 
-       for(int i = 0; i < arr.length; i++){
-         int oddsum = 0;
+    //    for(int i = 0; i < arr.length; i++){
+    //      int oddsum = 0;
+
+    //     for(int j = i; j < arr.length; j++){
+    //         int len = j - i + 1;
+    //         oddsum+=arr[j];
+    //         if(len % 2 == 1){
+    //             sum += oddsum;
+    //         }
+    //     }
+   
+    //    }
+    //    return sum;
+      
+      // optimal solutions
+
+      int[] prefix = new int[arr.length];
+      prefix[0] = arr[0];
+      int answer = 0;
+
+      for(int i = 1; i < arr.length; i++){
+        prefix[i] = prefix[i-1] + arr[i];
+      }
+
+      for(int i = 0; i < arr.length; i++){
+
 
         for(int j = i; j < arr.length; j++){
-            int len = j - i + 1;
-            oddsum+=arr[j];
-            if(len % 2 == 1){
-                sum += oddsum;
+            int length = j - i + 1;
+            int sum = 0;
+            if(i > 0){
+                sum = prefix[j] - prefix[i-1];
+            }
+            else{
+                sum = prefix[j];
+            }
+             
+            if(length % 2 == 1){
+               answer += sum;
             }
         }
-   
-       }
-       return sum;
-      
+      }
+      return answer;
     }
 }
