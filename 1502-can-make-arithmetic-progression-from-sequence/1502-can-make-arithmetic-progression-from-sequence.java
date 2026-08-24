104bdc479779732572813c6b5b8b095c244855e5
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-08-25 00:22:47 +0530

    Time: 6 ms (13.58%), Space: 44 MB (81.92%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.java
@@ -1,32 +1,35 @@
 class Solution {
     public boolean canMakeArithmeticProgression(int[] arr) {
-        Arrays.sort(arr);
-           ArrayList<Integer> list = new ArrayList<>();
-
-           for(int i = 0; i < arr.length-1; i++){
-            int difference = Math.abs(arr[i] - arr[i+1]);
-            list.add(difference);
-           }
-          int first = list.get(0);
-           for(int num : list){
-            if(first != num){
-                return false;
-            }
-           }
-           return true;
-
-
-
         // Arrays.sort(arr);
-        // int previous = 0;
-        // int next = 0;
-        // for(int i = 1; i < arr.length-1; i++){
-        //     previous = arr[i-1] - arr[i];
-        //     next = arr[i+1] - arr[i];
-        //     if(previous != next){
+        //    ArrayList<Integer> list = new ArrayList<>();
+
+        //    for(int i = 0; i < arr.length-1; i++){
+        //     int difference = Math.abs(arr[i] - arr[i+1]);
+        //     list.add(difference);
+        //    }
+        //   int first = list.get(0);
+        //    for(int num : list){
+        //     if(first != num){
         //         return false;
         //     }
-        // }
-        // return true;
+        //    }
+        //    return true;
+
+       // if(arr == null && arr.length == 0)return false;
+
+        if(arr.length < 3) {
+            return true;
+        }
+        Arrays.sort(arr);
+        int previous = 0;
+        int next = 0;
+        for(int i = 1; i < arr.length-1; i++){
+            previous = arr[i] - arr[i-1];
+            next = arr[i+1] - arr[i];
+            if(previous != next){
+                return false;
+            }
+        }
+        return true;
     }
 }
