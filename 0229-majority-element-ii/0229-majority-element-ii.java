a6a2b7315aaebd262a6d2c78f5e4817a70cff052
Mohan Raj D <120922329+mohanraj3-arj@users.noreply.github.com>
2026-08-28 13:28:43 +0530

    Time: 15 ms (28.69%), Space: 52.3 MB (80.25%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -4,24 +4,55 @@ class Solution {
         ArrayList<Integer> list = new ArrayList<>();
 
        
-        for(int i = 0; i < nums.length; i++){
-            int numCount = 0;
-            for(int j = i; j < nums.length; j++){
+        // for(int i = 0; i < nums.length; i++){
+        //     int numCount = 0;
+        //     for(int j = i; j < nums.length; j++){
 
-                    if(nums[i] == nums[j]){
-                        numCount++;
-                    }
+        //             if(nums[i] == nums[j]){
+        //                 numCount++;
+        //             }
 
-            }
-            if(numCount > appear){
-                if(!list.contains(nums[i])){
-                     list.add(nums[i]);
-                }
+        //     }
+        //     if(numCount > appear){
+        //         if(!list.contains(nums[i])){
+        //              list.add(nums[i]);
+        //         }
                
                 
+        //     }
+            
+        // }
+        // return list;
+
+
+        //*********** optimal approach *********** */
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+
+        for(int i = 0; i < nums.length; i++){
+           int val = map.get(nums[i]);
+           if(val > appear){
+
+            if(!list.contains(nums[i])){
+                list.add(nums[i]);
             }
+           }
             
         }
         return list;
+
+
+
+
+
+
+
+
+
+
     }
 }
